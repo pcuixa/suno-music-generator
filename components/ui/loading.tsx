739f67a5267d17d591c5512fa243bc8773a36a90
Peng Cui <peng.cui@hotmail.com>
2024-04-29 12:49:18 +0800

    update the loading component with new style

--- components/ui/loading.tsx
+++ components/ui/loading.tsx
@@ -1,17 +1,20 @@
-import * as React from "react"
+import * as React from "react";
 
 const Loading = () => {
+  const circleCommonClasses = "h-2.5 w-2.5 bg-current rounded-full";
+
   return (
-    <div className="flex items-center justify-center min-h-40 md:min-h-80">
-      <div className="relative">
-          <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
-          <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-yellow-500 animate-spin">
-          </div>
+    <div className="flex items-center justify-center h-screen">
+      {/* Centered loader */}
+      <div className="flex">
+        <div className={`${circleCommonClasses} mr-1 animate-bounce`}></div>
+        <div className={`${circleCommonClasses} mr-1 animate-bounce200`}></div>
+        <div className={`${circleCommonClasses} animate-bounce400`}></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-Loading.displayName = "Loading"
+Loading.displayName = "Loading";
 
-export { Loading }
+export { Loading };
